acc052f87318ca8648330a8b73858602e662a933
cosflow <andrescosflo@usal.es>
2026-03-12 15:14:10 +0100

    Arreglos en la conexión con el servidor de Qt y de Python

--- Raton/socket.c
+++ Raton/socket.c
@@ -14,7 +14,7 @@
 #include <pthread.h>
 #include <signal.h>
 
-#define BUFSIZE       256
+#define BUFSIZE       16
 #define REMOTE_PORT   45554
 #define LOCAL_PORT    45454
 //#define REMOTE_IP    "192.168.1.40"
@@ -25,130 +25,115 @@
 void * enviarPosRaton(void * arg);
 
 int main() {
-  char buf[1];
-  pthread_t hilo_Raton;
-  long timevar;
-  int opt = 1;
-  int addrlen = sizeof(struct sockaddr_in);
+	char buf[BUFSIZE];
+	pthread_t hilo_Raton;
+	long timevar;
+	int opt = 1;
+	int addrlen = sizeof(struct sockaddr_in);
+	struct sockaddr_in remoteAddr_in, localAddr_in;
+	int remoteSocket  = socket(AF_INET, SOCK_STREAM, 0);
+	int localSocket  = socket(AF_INET, SOCK_STREAM, 0);
 
-  int remoteSocket  = socket(AF_INET, SOCK_STREAM, 0);
-  int localSocket  = socket(AF_INET, SOCK_STREAM, 0);
-
-  if (remoteSocket == -1 || localSocket == -1) {
-    perror("Unable to create socket");
-    exit(1);
-  }
-
-  if (setsockopt(remoteSocket, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) == -1) {
-    perror("Error estableciendo el socket remoto sin delay");
-    exit(1);
-  }
-  if (setsockopt(localSocket, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) == -1) {
-    perror("Error estableciendo el socket local sin delay");
-    exit(1);
-  }
-
-  struct sockaddr_in remoteAddr_in, myAddr_in, localAddr_in;
-  memset(&remoteAddr_in, 0, sizeof(remoteAddr_in));
-  memset(&myAddr_in, 0, sizeof(myAddr_in));
-  memset(&myAddr_in, 0, sizeof(localAddr_in));
-
-  remoteAddr_in.sin_family = AF_INET;
-  remoteAddr_in.sin_port = htons(REMOTE_PORT);
-  if (inet_pton(AF_INET, REMOTE_IP, &remoteAddr_in.sin_addr) <= 0) {
-    perror("Error creando IP servidor remoto");
-    exit(1);
-  }
-
-  if (connect(remoteSocket, (struct sockaddr *)&remoteAddr_in, sizeof(remoteAddr_in)) == -1) {
-    perror("Error conectándose al servidor remoto");
-    exit(1);
-  }
-
-  if (getsockname(remoteSocket, (struct sockaddr *)&myAddr_in, &addrlen) == -1) {
-    perror("Unable to read socket address remoto");
-    exit(1);
-  }
-
-  time(&timevar);
-  printf("Conectado al servidor REMOTO %s desde puerto local %u a las %s",
-	 REMOTE_IP, ntohs(myAddr_in.sin_port), (char *)ctime(&timevar));
-
-  localAddr_in.sin_family = AF_INET;
-  localAddr_in.sin_port = htons(LOCAL_PORT);
-  if (inet_pton(AF_INET, LOCAL_IP, &localAddr_in.sin_addr) <= 0) {
-    perror("Error creando IP servidor local");
-    exit(1);
-  }
-
-  if (connect(localSocket, (struct sockaddr *)&localAddr_in, sizeof(localAddr_in)) == -1) {
-    perror("Error conectándose al servidor local");
-    exit(1);
-  }
-
-  printf("Conectado al servidor LOCAL %s:%d\n", LOCAL_IP, LOCAL_PORT);
-
-  if (pthread_create(&hilo_Raton, NULL, enviarPosRaton, (void*)(intptr_t)remoteSocket) != 0) {
-    perror("Error creando hilo de ratón");
-    return 1;
-  }
-
-  while (1) {
-    if (recv(remoteSocket, buf, sizeof(buf), 0) <= 0) {
-      perror("Error o cierre en recv remoto");
-      break;
-    }
-    printf("Recibido del remoto: %c\n", buf[0]);
-    if (send(localSocket,buf,sizeof(buf), 0) <=0) {
-      perror("Error o cierre en send local");
-      break;
-    }        
-  }
-
-    pthread_join(hilo_Raton, NULL);
-
-    time(&timevar);
-    printf("\nEjecución terminada a las: %s", (char *)ctime(&timevar));
-    close(remoteSocket);
-    close(localSocket);
-    return 0;
-  }
-
-  void * enviarPosRaton(void * arg) {
-    int remoteSocket =  (int)(intptr_t)arg;
-
-    struct input_event ev;
-    int dx = 0, dy = 0;
-    char buf[BUFSIZE];
-
-    int fd = open(MOUSE_DEV, O_RDONLY);
-    if (fd == -1) {
-      perror("Error al abrir el dispositivo de ratón.");
-      return NULL;
-    }
-
-    printf("Leyendo desplazamientos del ratón...\n");
-
-    while (1) {
-      if (read(fd, &ev, sizeof(struct input_event)) < (ssize_t)sizeof(struct input_event)) {
-	perror("Error al leer evento del ratón");
-	close(fd);
-	return NULL;
-      }
-
-      if (ev.type == EV_REL) {
-	if (ev.code == REL_X) {
-	  dx += ev.value;
-	} else if (ev.code == REL_Y) {
-	  dy += ev.value;
+	if (remoteSocket == -1 || localSocket == -1) {
+		perror("Unable to create socket");
+		exit(1);
 	}
 
-	sprintf(buf, "%d/%d\n", dx, dy);
-	printf("%s", buf);
-	send(remoteSocket, buf, strlen(buf), 0);
-      }
-    }
+	if (setsockopt(remoteSocket, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) == -1) {
+		perror("Error estableciendo el socket remoto sin delay");
+		exit(1);
+	}
+	if (setsockopt(localSocket, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) == -1) {
+		perror("Error estableciendo el socket local sin delay");
+		exit(1);
+	}
 
-    close(fd);
-    return NULL;
-  }
+	memset(&remoteAddr_in, 0, sizeof(remoteAddr_in));
+	memset(&localAddr_in, 0, sizeof(localAddr_in));
+
+	remoteAddr_in.sin_family = AF_INET;
+	remoteAddr_in.sin_port = htons(REMOTE_PORT);
+
+	if (inet_pton(AF_INET, REMOTE_IP, &remoteAddr_in.sin_addr) <= 0) {
+		perror("Error creando IP servidor remoto");
+		exit(1);
+	}
+
+	if (connect(remoteSocket, (struct sockaddr *)&remoteAddr_in, sizeof(remoteAddr_in)) == -1) {
+		perror("Error conectándose al servidor remoto");
+		exit(1);
+	}
+
+	time(&timevar);
+	printf("Conectado al servidor REMOTO %s a las %s\n", REMOTE_IP, (char *)ctime(&timevar));
+
+	localAddr_in.sin_family = AF_INET;
+	localAddr_in.sin_port = htons(LOCAL_PORT);
+	if (inet_pton(AF_INET, LOCAL_IP, &localAddr_in.sin_addr) <= 0) {
+		perror("Error creando IP servidor local");
+		exit(1);
+	}
+
+	if (connect(localSocket, (struct sockaddr *)&localAddr_in, sizeof(localAddr_in)) == -1) {
+		perror("Error conectándose al servidor local");
+		exit(1);
+	}
+
+	printf("Conectado al servidor LOCAL %s:%d\n", LOCAL_IP, LOCAL_PORT);
+	if (pthread_create(&hilo_Raton, NULL, enviarPosRaton, (void*)(intptr_t)remoteSocket) != 0) {
+		perror("Error creando hilo de ratón");
+		exit(2);
+	}
+	while (1) {
+		if (recv(remoteSocket, buf, BUFSIZE, 0) < 0) {
+			perror("Error o cierre en recv remoto");
+			exit(-1);
+		}
+		if (send(localSocket,buf,strlen(buf), 0) < 0) {
+			perror("Error o cierre en send local");
+			break;
+		} 
+	}
+
+	pthread_join(hilo_Raton, NULL);
+	time(&timevar);
+	printf("\nEjecución terminada a las: %s", (char *)ctime(&timevar));
+	close(remoteSocket);
+	close(localSocket);
+	return 0;
+}
+
+void * enviarPosRaton(void * arg) {
+	int remoteSocket =  (int)(intptr_t)arg;
+	struct input_event ev;
+	int dx = 0, dy = 0;
+	char buf[BUFSIZE];
+	int fd = open(MOUSE_DEV, O_RDONLY);
+
+	if (fd == -1) {
+		perror("Error al abrir el dispositivo de ratón.");
+		return NULL;
+	}
+	printf("Leyendo desplazamientos del ratón...\n");
+	while (1) {
+		if (read(fd, &ev, sizeof(struct input_event)) < (ssize_t)sizeof(struct input_event)) {
+			perror("Error al leer evento del ratón");
+			close(fd);
+			return NULL;
+		}
+
+		if (ev.type == EV_REL) {
+			if (ev.code == REL_X) {
+				dx += ev.value;
+			} else if (ev.code == REL_Y) {
+				dy += ev.value;
+			}
+
+			snprintf(buf, BUFSIZE, "%d/%d\n", dx, dy);
+			send(remoteSocket, buf, strlen(buf), 0);
+		}
+	}
+
+	close(fd);
+	return NULL;
+}
--- Vibrar/vibrar.py
+++ Vibrar/vibrar.py
@@ -12,21 +12,32 @@ PORT = 45454        # LOCALPORT
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        print("Cliente conectado:", self.client_address)
+        """
+        i2c = busio.I2C(board.SCL, board.SDA)
+        if not adafruit_drv2605._DRV2605_ADDR in i2c.scan():
+            print("Dispositivo 2065 no encontrado")
+            sys.exit()
+
+        drv = adafruit_drv2605.DRV2605(i2c)
+        """
+        while True:
+            data = self.request.recv(16).strip()
+            if not data:
+                print("Cliente desconectado")
+                self.server.shutdown()
+                self.server.server_close()
+                break
+
+            print(data.decode(errors="ignore"))
+            """
+            drv.sequence[0] = adafruit_drv2605.Effect(data)
+            drv.play()  # play the effect
+            time.sleep(0.5)  # for 0.5 seconds
+            drv.stop()
+            """
 
 if __name__ == "__main__":
-
-    """
-    i2c = busio.I2C(board.SCL, board.SDA)
-    if not adafruit_drv2605._DRV2605_ADDR in i2c.scan():
-	    print("Dispositivo 2065 no encontrado")
-	    sys.exit()
-
-    drv = adafruit_drv2605.DRV2605(i2c)
-    """
 
     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
         server.serve_forever()
