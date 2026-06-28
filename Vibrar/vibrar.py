24080ee8bdb3c4127f371deaae7613ebb10fd206
Andrés Cosme Flores <andrewcosflow@gmail.com>
2026-06-28 16:13:26 +0000

    Descomentar DRV

--- Vibrar/vibrar.py
+++ Vibrar/vibrar.py
@@ -10,14 +10,12 @@ import struct
 HOST = '127.0.0.1'  # localhost
 PORT = 45454        # LOCALPORT
 
-"""
 i2c = busio.I2C(board.SCL, board.SDA)
 if not adafruit_drv2605._DRV2605_ADDR in i2c.scan():
     print("Dispositivo 2065 no encontrado")
     sys.exit()
 
 drv = adafruit_drv2605.DRV2605(i2c)
-"""
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -34,10 +32,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 break
             vibId = struct.unpack("!I", data)[0]  # network order -> int
             print(vibId)
-            """
+            
             drv.sequence[0] = adafruit_drv2605.Effect(vibId)
-            drv.play()  # play the effect
-            time.sleep(0.5)  # for 0.5 seconds
+            drv.play()
+            time.sleep(0.5)
             drv.stop()
-            """
+            
 
